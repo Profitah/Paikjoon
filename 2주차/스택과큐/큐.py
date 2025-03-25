c9f5edc3c3dcdcf15252e19bf3172f5efe9d7679
이윤아 <qwer120542@gmail.com>
2025-03-25 10:33:27 +0900

    어쩌면 교수님이 틀리신게 아닐까요?

--- 2주차/스택과큐/큐.py
+++ 2주차/스택과큐/큐.py
@@ -2,11 +2,11 @@ import sys
 input = sys.stdin.readline
 
 import collections
-n = int(input())
-queue = collections.deque()
+n = int(input()) # 정수 n개를 입력받고 
+queue = collections.deque() # deque를 사용하여 큐를 만든다. # 여기서 deque란, 양방향으로 데이터를 추가하거나 뺄 수 있는 queue이다.
 
-for i in range(n):
-    command = input().split()
+for i in range(n): # n번 반복한다.
+    command = input().split() # 명령어를 입력받는다.
 
     if command[0] == 'push': # 큐에 정수를 넣는다.
         queue.append(command[1]) # append() 함수를 사용하여 큐에 정수를 넣는다.
@@ -15,17 +15,17 @@ for i in range(n):
         if len(queue)==0: # 큐가 완전히 비어있을 때 
             print(-1) # -1을 출력한다.
         else:
-            print(queue.popleft())
-    elif command[0] == 'size':
-        print(len(queue))
-    elif command[0] == 'empty':
-        if len(queue)==0:
+            print(queue.popleft()) # popleft() 함수를 사용하여 큐에서 정수를 빼고, 빼낸 정수를 출력한다.
+    elif command[0] == 'size': # 큐의 크기를 출력한다.
+        print(len(queue)) # 큐의 길이를 출력한다.
+    elif command[0] == 'empty': # 큐가 비어있으면 1, 아니면 0을 출력한다.
+        if len(queue)==0: # 만약에 큐가 완전히 비어있다면 
             print(1)
         else:
             print(0)
-    elif command[0] == 'front':
-        if len(queue)==0:
-            print(-1)
+    elif command[0] == 'front': # 큐의 가장 앞에 있는 정수를 출력한다.
+        if len(queue)==0: # 큐가 완전히 비어있을 때는
+            print(-1) # -1을 출력한다.
         else:
             print(queue[0])
     else: 
