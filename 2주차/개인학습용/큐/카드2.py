1635233dfb00ed5f296571744ce077284f5b70e2
이윤아 <qwer120542@gmail.com>
2025-03-26 10:40:59 +0900

    card2

--- 2주차/개인학습용/큐/카드2.py
+++ 2주차/개인학습용/큐/카드2.py
@@ -1,21 +1,12 @@
-"""
-N장의 카드가 있다. 각각의 카드는 차례로 1부터 N까지의 번호가 붙어 있으며, 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다.
-
-이제 다음과 같은 동작을 카드가 한 장 남을 때까지 반복하게 된다. 우선, 제일 위에 있는 카드를 바닥에 버린다. 그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
-
-예를 들어 N=4인 경우를 생각해 보자. 카드는 제일 위에서부터 1234 의 순서로 놓여있다. 1을 버리면 234가 남는다. 여기서 2를 제일 아래로 옮기면 342가 된다. 3을 버리면 42가 되고, 4를 밑으로 옮기면 24가 된다. 마지막으로 2를 버리고 나면, 남는 카드는 4가 된다.
-
-N이 주어졌을 때, 제일 마지막에 남게 되는 카드를 구하는 프로그램을 작성하시오."""
-
 from collections import deque
 
-def find_last_card(N): # N장의 카드가 있으면
-    queue = deque(range(1, N+1)) # 1부터 N이라는 값을 가지고 있는 수를 deque로 만들어 # deque는 양방향으로 데이터를 추가하거나 뺄 수 있는 queue
+def my_card(N): # N장의 카드를 입력받아 실행되는 함수
+    q = deque(range(1, N+1)) # 1부터 N이라는 값을 가지고 있는 수를 deque로 만들어 # deque는 양방향으로 데이터를 추가하거나 뺄 수 있는 queue
 
-    while len(queue) > 1: # queue의 길이가 1보다 크면
-        queue.popleft() # queue의 맨 앞에 있는 값을 제거하고
-        queue.append(queue.popleft()) # queue의 맨 앞에 있는 값을 제거했다가 그 값을 queue의 맨 뒤에 추가한다.
-    return queue[0] # 그리고 queue의 맨 앞에 있는 값을 반환한다.
+    while len(q) > 1: # queue의 길이가 1보다 크면
+        q.popleft() # queue의 맨 앞에 있는 값을 제거하고
+        q.append(q.popleft()) #새롭게 생긴 큐 맨 앞 값을 queue의 맨 뒤에 추가한다.
+    return q[0] # 그리고 마지막에 남은 값을 반환한다.
 
 N = int(input()) # N을 입력받고
-print(find_last_card(N))
+print(my_card(N)) # my_card 함수 결과값을 출력하라.
---
+++ 2주차/개인학습용/큐/카드2_1.py
@@ -0,0 +1,12 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+N = int(input()) # N을 입력받고
+
+q = deque(range(1, N+1)) # 1부터 N이라는 값을 가지고 있는 수를 deque로 만들어 # deque는 양방향으로 데이터를 추가하거나 뺄 수 있는 queue
+
+while len(q) > 1: # queue의 길이가 1보다 크면
+    q.popleft() # queue의 맨 앞에 있는 값을 제거하고
+    q.append(q.popleft()) #새롭게 생긴 큐 맨 앞 값을 queue의 맨 뒤에 추가한다.
+print(q[0]) # 그리고 마지막에 남은 값을 출력한다.
