8459cfff007de0b77f7c16e6f51c50421484555b
이윤아 <qwer120542@gmail.com>
2025-03-27 08:35:13 +0900

    오잉

--- 2주차/개인학습용/큐/탑_요소뒤집어서.py
+++ 2주차/개인학습용/큐/탑_요소뒤집어서.py
@@ -1,20 +1,22 @@
 import sys
+input = sys.stdin.readline
 
-input = sys.stdin.readline # 빠른 입력
-N = int(input())  # 탑의 개수 N
-TowerHeights = list(map(int, input().split()))  # 탑들의 높이를 입력받는다
+# 입력처리
+N = int(input())  # 탑들의 개수
+N2 = list(map(int, input().split()))  # 탑들의 높이를 입력받는다
 stack = []  # 스택 생성
 solution = [0] * N  # 스택을 0으로 초기화하고, 입력받을 개수만큼 크기 늘리기
+tower_h = N2[::-1]  # N2를 뒤집어서 저장한다
 
-for i in range(N):  # 사용자에게 입력받은 만큼 반복한다.
+for i in range(N):  # 사용자에게 입력받은 만큼 반복한다
     # 현재 탑보다 높은 탑이 있을 때까지 스택에서 꺼낸다
-    while stack and TowerHeights[i] >= TowerHeights[stack[-1]]:  # stack[-1]은 현재 스택의 인덱스 stack[i]는 현재 탑의 높이
-        stack.pop()  # 현재 탑보다 작은 탑을 배열에서 제거
+    while stack and tower_h[i] >= stack[-1][0]:  # stack[-1][0]은 탑의 높이
+        stack.pop()
     
     if stack:
-        solution[i] = stack[-1] + 1  # 스택에 있는 마지막 탑의 번호를 solution에 저장 
+        solution[i] = stack[-1][1]  # stack[-1][1]은 탑의 번호
     
-    stack.append(i)  # 현재 탑의 인덱스를 스택에 추가
+    stack.append((tower_h[i], i + 1))  # (탑의 높이, 탑의 번호) 쌍을 스택에 저장
 
-# 결과 출력
+# 출력
 print(*solution)  # solution 배열을 공백을 두고 출력
