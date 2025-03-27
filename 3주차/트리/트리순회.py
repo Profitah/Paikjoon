4972fd41e7f512e8a360ccbb4b5df41bd31ef358
이윤아 <qwer120542@gmail.com>
2025-03-27 19:43:25 +0900

    null값 방어

--- 3주차/트리/트리순회.py
+++ 3주차/트리/트리순회.py
@@ -9,17 +9,17 @@ for N in range(N):
     tree[root] = [left, right]
 
 def preorder(root):
-    if root !=  "." :
+    if root and root != "."	:
         print(root , end="")
 
 def inorder(root):
-    if root != '.':
+    if root and root != "."	:
         print(root, end='')  
         inorder(tree[root][0])  
         inorder(tree[root][1])  
  
 def postorder(root):
-    if root != '.':
+    if root and root != "."	:
         print(root, end='') 
         postorder(tree[root][0]) 
         postorder(tree[root][1])
