8c65d14f6aa8255d8aa73b915fda6ec6d122a128
이윤아 <qwer120542@gmail.com>
2025-03-29 11:31:08 +0900

    dfs와 bfs 드디어 마지막git add .git add .git add .git add .

---
+++ 3주차/DFS와BFS/Dfs와bfs.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+n, m, v = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+방문 = [False] * (n+1)
+
+for _ in range(m):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 방문 순서를 보장하기 위해 정렬
+for i in range(1, n+1):
+    graph[i].sort()
+
+def dfs(visit):
+    방문[visit] = True
+    print(visit, end=' ')  # ✅ 공백 추가
+    for i in graph[visit]:
+        if not 방문[i]:
+            dfs(i)
+
+def bfs(visit):
+    q = deque([visit])
+    방문[visit] = True
+    while q:
+        qpop = q.popleft()
+        print(qpop, end=' ')  
+        for i in graph[qpop]:
+            if not 방문[i]:
+                방문[i] = True
+                q.append(i)
+
+dfs(v)
+print() 
+방문 = [False] * (n + 1)
+bfs(v)
