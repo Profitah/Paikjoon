34bcc81ad15b9ecdf15352027fe0ddbd85ba494a
이윤아 <qwer120542@gmail.com>
2025-04-01 18:20:15 +0900

    연결요소의 개수

--- 3주차/미로탐색/미로탐색다시.py
+++ 3주차/미로탐색/미로탐색다시.py
@@ -1,1 +1,23 @@
 # 백준 2178
+
+from collections import deque
+
+# 미로의 크기 입력받기
+rows, cols = map(int, input().split())
+# 미로 지도 입력받기 (2차원 리스트)
+maze = []
+    for i in range(rows):
+    
+
+
+
+
+# 이동할 수 있는 네 방향 (상, 하, 좌, 우)
+maze_row = [1, -1, 0, 0]
+maze_col = []
+
+# BFS로 최단 거리 찾기
+def bfs (rows, cols):
+
+
+return 
